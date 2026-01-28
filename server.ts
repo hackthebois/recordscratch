6394987879a1f211ba7dbbaab44e8a0589812bb5
Billy Hawkes <billyhawkes02@gmail.com>
2026-01-27 23:18:21 -0500

    fix: production styling with mime types

--- server.ts
+++ server.ts
@@ -6,16 +6,30 @@ const handleRequest = createRequestHandler({ build: SERVER_BUILD_DIR });
 
 const port = process.env.PORT || 3000;
 
+const getContentType = (pathname: string) => {
+	if (pathname.endsWith(".css")) return "text/css";
+	if (pathname.endsWith(".js")) return "application/javascript";
+	if (pathname.endsWith(".map")) return "application/json";
+	if (pathname.endsWith(".html")) return "text/html";
+	if (pathname.endsWith(".png")) return "image/png";
+	if (pathname.endsWith(".svg")) return "image/svg+xml";
+	return "application/octet-stream";
+};
+
 Bun.serve({
 	port: process.env.PORT || 3000,
 	async fetch(req) {
 		const url = new URL(req.url);
-		console.log("Request URL:", url.pathname);
 
 		const staticPath = url.pathname === "/" ? "/index.html" : url.pathname;
 		const file = Bun.file(CLIENT_BUILD_DIR + staticPath);
 
-		if (await file.exists()) return new Response(await file.arrayBuffer());
+		if (await file.exists())
+			return new Response(await file.arrayBuffer(), {
+				headers: {
+					"Content-Type": getContentType(staticPath),
+				},
+			});
 
 		return handleRequest(req);
 	},
