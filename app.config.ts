eb775f2d7ae21329f66a4faaf68ddcbda0dcba52
Billy Hawkes <billyhawkes02@gmail.com>
2026-02-03 19:17:25 -0500

    task: update version

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import type { ConfigContext, ExpoConfig } from "@expo/config";
 
-const version = "0.1.0";
+const version = "0.1.1";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
